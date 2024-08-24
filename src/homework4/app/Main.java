f5ae52747ac218f76678c677184fd3ff37536d90
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-08-24 15:10:38 +0300

    Add fuctional to Main class

--- src/homework4/app/Main.java
+++ src/homework4/app/Main.java
@@ -2,6 +2,24 @@ package homework4.app;
 
 public class Main {
     public static void main(String[] args) {
+        Customer customer = getCusomer(getData());
+        String customerInfo = "Customer: " + customer.getName() +
+                ", phone " + customer.getPhone();
+        getOutput(customerInfo);
+    }
 
+    public static String[] getData(){
+        return new String[]{"Joe Ronald", "+43032992842"};
+    }
+
+    public static Customer getCusomer(String[] dataArray){
+        return new Customer(dataArray[0], dataArray[1]);
+    }
+
+    public static void getOutput(String outputData){
+        System.out.println(outputData);
     }
 }
+
+
+
