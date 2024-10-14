7ad81d472ecb4cc418cadf157fa92a2cdae9e441
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-10-14 15:45:15 +0300

    Add homework 14

---
+++ src/homework14/DataRepository.java
@@ -0,0 +1,19 @@
+package homework14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRepository {
+
+    public List<String> getData() {
+        List<String> list = new ArrayList<>();
+
+        list.add("Alice");
+        list.add("Bob");
+        list.add("Lucy");
+        list.add("Denis");
+        list.add("Tom");
+
+        return list;
+    }
+}
---
+++ src/homework14/UIOperator.java
@@ -0,0 +1,8 @@
+package homework14;
+
+public class UIOperator {
+
+    public void getOutput(String output) {
+        System.out.println(output);
+    }
+}
